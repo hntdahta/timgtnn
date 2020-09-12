f861129f012a362e088c7159a0132189ab881f75
Thanh Dat <datmathoingu@gmail.com>
2020-09-13 06:25:29 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Text;
 
-namespace timgtnn
+namespace gtnn
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            int size;
+            int[] array;
+            Console.WriteLine("Nhập kích thước mảng: ");
+            size = Int32.Parse(Console.ReadLine());
+
+            array = new int[size];
+            int i = 0;
+            while (i < array.Length)
+            {
+                Console.WriteLine("Nhập phần tử của mảng " + (i + 1) + " : ");
+                array[i] = Int32.Parse(Console.ReadLine());
+                i++;
+            }
+            Console.WriteLine("Property list: ");
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.WriteLine(array[j] + "\t");
+            }
+            int min = MinValue(array);
+            Console.WriteLine("Phần tử nhỏ nhất trong mảng là :{0}", min);
+
+        }
+        static int MinValue(int[] array)
+        {
+
+            int min = array[0];
+            int index = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = i;
+                }
+            }
+            return min;
         }
     }
 }
